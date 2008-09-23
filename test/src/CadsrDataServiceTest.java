156267f6342061916bc5e0c42df0ff775f2204b8
ludetc <ludetc>
2008-09-23 15:14:50 +0000

    test files.
    SVN-Revision: 25

---
+++ test/src/CadsrDataServiceTest.java
@@ -0,0 +1,86 @@
+import gov.nih.nci.cagrid.cqlquery.CQLQuery;
+import gov.nih.nci.cagrid.data.client.DataServiceClient;
+import java.util.Iterator;
+
+import gov.nih.nci.cagrid.common.Utils;
+
+import gov.nih.nci.cagrid.cqlresultset.CQLQueryResults;
+
+import gov.nih.nci.cagrid.data.utilities.CQLQueryResultsIterator;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+
+public class CadsrDataServiceTest {
+  
+  private String serviceUrl;
+  private String queryDirectory;
+  
+  public CadsrDataServiceTest(String serviceUrl, String queryDirectory) {
+    this.serviceUrl = serviceUrl;
+    this.queryDirectory = queryDirectory;
+  }
+  
+  private void performQuery() {
+    try {
+      // initialize the generic data service client
+      DataServiceClient client = new DataServiceClient(serviceUrl);
+
+      
+      String[] filenames = new File(queryDirectory).list(new FilenameFilter() {
+          public boolean accept(File dir, String name) {
+            return name.endsWith(".xml");
+          }
+        });
+      
+      if(filenames == null) 
+        filenames = new String[0];
+      
+      if(filenames.length == 0) {
+        System.out.println("No query files. Exiting");
+        System.exit(0);
+      }
+
+      for(String queryFilename : filenames) {
+
+        // deserialize the CQL query
+        CQLQuery query = (CQLQuery) Utils.deserializeDocument(queryDirectory + "/" + queryFilename, CQLQuery.class);
+        // execute the query on the data service
+        System.out.println("Querying file:  " + queryFilename);
+        long l = new java.util.Date().getTime();
+        CQLQueryResults results = client.query(query);
+        l = (new java.util.Date().getTime() - l);
+        System.out.println("Time to run Query : " + l  + " ms");
+
+        // create a results iterator
+        System.out.println("Iterating");
+        Iterator iter = new CQLQueryResultsIterator(results, true);
+        // iterate and print XML
+        System.out.println("-- RESULT --");
+        while (iter.hasNext()) {
+          String value = (String) iter.next();
+          //           System.out.println(value);
+          System.out.print("+");
+        }
+        System.out.println("");
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+  
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.err.println("USAGE: <serviceUrl> <queryDirectory>");
+      System.exit(1);
+    }
+
+    CadsrDataServiceTest runner = new CadsrDataServiceTest(args[0], args[1]);
+    runner.performQuery();
+  }
+
+}
